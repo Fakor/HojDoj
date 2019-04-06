9355c0e4cea6a28d229548e0aac399467390ff0e
david <davidjohansson35@gmail.com>
2019-04-06 12:26:20 +0200

    Added something that might work as layrs to sketch

--- hojdoj/sketch.py
+++ hojdoj/sketch.py
@@ -4,4 +4,23 @@ import tkinter as tk
 class Sketch(tk.Canvas):
     def __init__(self, parent, width, height):
         tk.Canvas.__init__(self, parent, width=width, height=height)
+        self._layers = []
         self.pack()
+        # draw a circle on layer 1:
+        self.add_to_layer(1, self.create_oval, (100,10,30,40), outline="red")
+        # draw a square on layer 2:
+        self.add_to_layer(2, self.create_rectangle, (10,10,50,60), fill="blue")
+        print(self._layers)
+
+    def add_to_layer(self, layer, command, cords, **kwargs):
+        layer_tag = "layer %s" % layer
+        if layer_tag not in self._layers: self._layers.append(layer_tag)
+        tags = kwargs.setdefault("tags", [])
+        tags.append(layer_tag)
+        item_id = command(cords, **kwargs)
+        self._adjust_layers()
+        return item_id
+
+    def _adjust_layers(self):
+        for layer in sorted(self._layers):
+            self.lift(layer)
