3a4389130ff50467a90a40a58c5e83dbd7f0eebd
david <davidjohansson35@gmail.com>
2019-04-07 20:16:21 +0200

    Can now write lines with mouse

--- hojdoj/sketch.py
+++ hojdoj/sketch.py
@@ -4,23 +4,24 @@ import tkinter as tk
 class Sketch(tk.Canvas):
     def __init__(self, parent, width, height):
         tk.Canvas.__init__(self, parent, width=width, height=height)
-        self._layers = []
         self.pack()
-        # draw a circle on layer 1:
-        self.add_to_layer(1, self.create_oval, (100,10,30,40), outline="red")
-        # draw a square on layer 2:
-        self.add_to_layer(2, self.create_rectangle, (10,10,50,60), fill="blue")
-        print(self._layers)
 
-    def add_to_layer(self, layer, command, cords, **kwargs):
-        layer_tag = "layer %s" % layer
-        if layer_tag not in self._layers: self._layers.append(layer_tag)
-        tags = kwargs.setdefault("tags", [])
-        tags.append(layer_tag)
-        item_id = command(cords, **kwargs)
-        self._adjust_layers()
-        return item_id
+        self.bind("<Button-1>", self.on_button_press)
+        self.bind("<B1-Motion>", self.on_move_press)
+        self.bind("<ButtonRelease-1>", self.on_button_release)
 
-    def _adjust_layers(self):
-        for layer in sorted(self._layers):
-            self.lift(layer)
+        self.start_point = None
+        self.current_line = None
+
+        self.objects = []
+
+    def on_button_press(self, event):
+        self.start_point = (event.x, event.y)
+        self.current_line = self.create_line(*self.start_point, *self.start_point)
+        self.objects.append(self.current_line)
+
+    def on_move_press(self, event):
+        self.coords(self.current_line, *self.start_point, event.x, event.y)
+
+    def on_button_release(self, event):
+        pass
