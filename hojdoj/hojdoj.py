61b2814f9370a91014b81bca437d8c3c42f8af43
david <davidjohansson35@gmail.com>
2019-04-09 20:37:34 +0200

    Moved variables to main for better sooping

--- hojdoj/hojdoj.py
+++ hojdoj/hojdoj.py
@@ -1,11 +1,35 @@
 import tkinter as tk
 
 import main_window
+from scrollable_output import ScrollableOutput
+from sketch import Sketch
+
+
+def enter_pressed(console):
+    line_private___ = console.get(1.4, tk.END).strip()
+    if line_private___[-1] != ":":
+        console.shell.runcode(line_private___)
+
+
+def quit_hojdoj(rt):
+    print("Quiting HojDoj!!")
+    rt.quit()
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.config(background="red")
-    main_window = main_window.MainWindow(root)
+
+    main_window = main_window.MainWindow(root, locals=locals())
+    main_window.bind('<Return>', lambda eff: enter_pressed(main_window))
+    main_window.bind('<Control-c>', lambda eff: quit_hojdoj(root))
+
+    output_window = tk.Toplevel(root)
+    output = ScrollableOutput(output_window)
+    output.grid()
+
+    sketch_window = tk.Toplevel(root)
+    sk = Sketch(sketch_window, 300, 300, "sk", output=output)
+    sk.grid()
 
     root.mainloop()
--- hojdoj/main_window.py
+++ hojdoj/main_window.py
@@ -1,34 +1,13 @@
 import tkinter as tk
 import code
 
-from sketch import Sketch
-from scrollable_output import ScrollableOutput
-
 
 class MainWindow(tk.Text):
-    def __init__(self, parent):
+    def __init__(self, parent, locals):
         self.parent = parent
         tk.Text.__init__(self, parent)
-        self.bind('<Return>', self.enter_pressed)
-        self.bind('<Control-c>', self.quit)
 
         self.grid()
         self.insert(0.0, ">>> ")
 
-        self.output_window = tk.Toplevel(self.parent)
-        self.output = ScrollableOutput(self.output_window)
-        self.output.grid()
-
-        self.sketch_window = tk.Toplevel(self.parent)
-        self.sketch = Sketch(self.sketch_window, 300, 300, "sketch", output=self.output)
-        self.sketch.grid()
-
-        self.shell = code.InteractiveInterpreter(locals=locals())
-
-    def enter_pressed(self, event):
-        line_private___ = self.get(1.4, tk.END).strip()
-        if line_private___[-1] != ":":
-            self.shell.runcode(line_private___)
-
-    def quit(self, event):
-        self.parent.quit()
+        self.shell = code.InteractiveInterpreter(locals=locals)
