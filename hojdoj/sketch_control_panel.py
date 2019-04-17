d857f717c12afb67a1244a03c51390f8cdf55522
david <davidjohansson35@gmail.com>
2019-04-17 21:54:22 +0200

    Conveniance function for adding images to sketch_control_panel

--- hojdoj/sketch_control_panel.py
+++ hojdoj/sketch_control_panel.py
@@ -6,6 +6,7 @@ import Commands
 B_WIDTH = 70
 B_HEIGHT = 70
 
+COLUMNS = 2
 
 class SketchControlPanel(tk.Frame):
     def __init__(self, parent, sketch):
@@ -13,28 +14,20 @@ class SketchControlPanel(tk.Frame):
         self.parent = parent
         self.sketch = sketch
 
-        self.balt_img = PIL.Image.open(self.sketch.image_paths.baltazar)
-        self.balt_img = self.balt_img.resize((B_WIDTH, B_HEIGHT), PIL.Image.ANTIALIAS)
-        self.balt_image_button = PIL.ImageTk.PhotoImage(self.balt_img)
+        self.image_row = 1
+        self.image_col = 0
 
-        self.tummen_img = PIL.Image.open(self.sketch.image_paths.tummen)
-        self.tummen_img = self.tummen_img.resize((B_WIDTH, B_HEIGHT), PIL.Image.ANTIALIAS)
-        self.tummen_image_button = PIL.ImageTk.PhotoImage(self.tummen_img)
+        self.p_images = []
+
+        self.add_image_button(self.sketch.image_paths.baltazar)
+        self.add_image_button(self.sketch.image_paths.tummen)
 
         line_button = tk.Button(self, text="Line", command=self.line_tool_active)
         rect_button = tk.Button(self, text="Rectangle", command=self.rect_tool_active)
-        balt_button = tk.Button(self, image=self.balt_image_button,
-                                command=lambda: self.image_tool_active(self.sketch.image_paths.baltazar),
-                                height=B_HEIGHT, width=B_WIDTH)
-        tummen_button = tk.Button(self, image=self.tummen_image_button,
-                                command=lambda: self.image_tool_active(self.sketch.image_paths.tummen),
-                                height=B_HEIGHT, width=B_WIDTH)
 
 
         line_button.grid(row=0, column=0)
         rect_button.grid(row=0, column=1)
-        balt_button.grid(row=1, column=0)
-        tummen_button.grid(row=1, column=1)
 
         blue_button = tk.Button(self, bg="blue", command=self.color_blue_active)
         red_button = tk.Button(self, bg="red", command=self.color_red_active)
@@ -57,3 +50,19 @@ class SketchControlPanel(tk.Frame):
 
     def color_red_active(self):
         self.sketch.fill_color = "red"
+
+    def add_image_button(self, path):
+        img = PIL.Image.open(path)
+        img = img.resize((B_WIDTH, B_HEIGHT), PIL.Image.ANTIALIAS)
+        image_button = PIL.ImageTk.PhotoImage(img)
+        self.p_images.append(image_button)
+        button = tk.Button(self, image=self.p_images[-1],
+                                command=lambda: self.image_tool_active(path),
+                                height=B_HEIGHT, width=B_WIDTH)
+        button.grid(row=self.image_row, column=self.image_col)
+        if self.image_col == COLUMNS - 1:
+            self.image_col = 0
+            self.image_row = self.image_row + 1
+        else:
+            self.image_col = self.image_col + 1
+
