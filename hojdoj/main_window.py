30f29d328ee00d760007c745b601dae904dd6f74
david <davidjohansson35@gmail.com>
2019-05-19 21:08:57 +0200

    Help variables added for setting app dimensions

--- hojdoj/main_window.py
+++ hojdoj/main_window.py
@@ -10,16 +10,25 @@ import main_control_panel
 class MainWindow(tk.Frame):
     def __init__(self, parent, image_root, width, height):
         tk.Frame.__init__(self, parent, width=width, height=height)
-        self.update()
-        parent.update()
+
+        console_height = int(height/5)
+        sketch_height = height-console_height
+        sketch_control_width = int(width/10)
+        main_control_width = int(width/10)
+        sketch_width = width - sketch_control_width - main_control_width
+        command_window_width = int(width/2)
+        output_window_width = width - command_window_width
+
+        main_control_x = sketch_control_width + sketch_width
+
         output_window = scrollable_output.ScrollableOutput(self)
         sk = sketch.Sketch(self, "sk", image_root, output=output_window)
         sketch_control = sketch_control_panel.SketchControlPanel(self, sk)
         main_control = main_control_panel.MainControlPanel(self, parent)
         command_window = command_terminal.CommandTerminal(self, locals())
 
-        sketch_control.place(x=0,y=0, width=int(width/8), height=int(height*4/5))
-        sk.place(x=int(width/8),y=0, width=int(width*5/7), height=int(height*4/5))
-        main_control.place(x=int(width*7/8),y=0, width=int(width/7), height=int(height*3/4))
-        command_window.place(x=0,y=int(height*4/5), width=int(width/2), height=int(height/5))
-        output_window.place(x=int(width/2), y=int(height*4/5), width=int(width / 2), height=int(height / 5))
+        sketch_control.place(x=0,y=0, width=sketch_control_width, height=sketch_height)
+        sk.place(x=sketch_control_width,y=0, width=sketch_width, height=sketch_height)
+        main_control.place(x=main_control_x,y=0, width=main_control_width, height=sketch_height)
+        command_window.place(x=0,y=sketch_height, width=command_window_width, height=console_height)
+        output_window.place(x=command_window_width, y=sketch_height, width=output_window_width, height=console_height)
