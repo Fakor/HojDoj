034227baeb3aeacc6a701cec0de3257a7873ced6
david <davidjohansson35@gmail.com>
2019-06-24 22:44:48 +0200

    Command Terminal now only have one entry

--- hojdoj/command_terminal.py
+++ hojdoj/command_terminal.py
@@ -10,30 +10,27 @@ class InputEntry(tk.Entry):
 
     def enter_pressed(self, event):
         self.parent.run_command(self.get())
-        self.delete(0,tk.END)
 
 
 class CommandTerminal(tk.Frame):
     def __init__(self, parent, loc):
         tk.Frame.__init__(self, parent)
-        self.content = tk.StringVar()
         self.input = InputEntry(self)
-        self.output = tk.Entry(self, textvariable=self.content)
 
         self.input.grid(row=0,column=0)
-        self.output.grid(row=1,column=0)
 
         self.shell = code.InteractiveInterpreter(locals=loc)
 
     def add_command(self, text):
-        self.content.set(text)
+        self.input.delete(0, tk.END)
+        self.input.insert(0, text)
+
+    def enter_pressed(self, event):
+        self.shell.runcode(self.input.get())
+
+    def place(self, **kwargs):
+        self.input.config(width=int(kwargs["width"]))
+        tk.Frame.place(self, **kwargs)
 
     def run_command(self, text):
         self.shell.runcode(text)
-
-    def place(self, **kwargs):
-        self.output.config(width=int(kwargs["width"]/2))
-
-        self.input.config(width=int(kwargs["width"] / 2))
-
-        tk.Frame.place(self, **kwargs)
