a942828cf39c6100beff231f89408d8110527251
david <davidjohansson35@gmail.com>
2019-07-03 20:04:37 +0200

    Implementation for DeleteInteractive

--- hojdoj/delete_interactive.py
+++ hojdoj/delete_interactive.py
@@ -3,9 +3,11 @@
 class DeleteInteractive:
     def __init__(self, sketch, event):
         self.sketch = sketch
+        self.sketch.mark_object(event.x, event.y)
+        self.sketch.delete_command(self.sketch.marked_object_index)
 
     def on_move(self, event):
-        print('Move', event.x, event.y)
+        pass
 
     def on_release(self, event):
-        print('Release', event.widget)
+        pass
