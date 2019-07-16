93e8b9b1902497743c63200294cf39d57145cf25
david <davidjohansson35@gmail.com>
2019-07-16 21:12:36 +0200

    Screen info fixed through DTools

--- hojdoj/hojdoj.py
+++ hojdoj/hojdoj.py
@@ -4,17 +4,15 @@ import sys
 
 import main_window
 import configuration
-from screeninfo import get_monitors
+import DTools.tk_tools
+
+
+SCREEN_INDEX = 0
 
 
 def quit_hojdoj(rt):
     print("Quiting HojDoj!!")
     rt.quit()
-
-
-def get_main_screen_conf():
-    main_monitor = get_monitors()[0]
-    return main_monitor.width, main_monitor.height, main_monitor.x, main_monitor.y
 
 
 if __name__ == '__main__':
@@ -27,10 +25,9 @@ if __name__ == '__main__':
 
     root.attributes("-fullscreen", True)
 
-    width, height, x, y = get_main_screen_conf()
-    root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    root.geometry(DTools.tk_tools.get_screen_geometry(SCREEN_INDEX))
 
-    main_window = main_window.MainWindow(root, config, width=width, height=height)
+    main_window = main_window.MainWindow(root, config, *DTools.tk_tools.get_screen_size(SCREEN_INDEX)) #, width=width, height=height)
     main_window.pack()
 
     root.bind('<<quit_now>>', lambda eff: quit_hojdoj(root))
