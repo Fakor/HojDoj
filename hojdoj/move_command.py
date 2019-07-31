75932487d2acf4017bf53471a84bd56f08bbf6ae
david <davidjohansson35@gmail.com>
2019-07-31 22:30:29 +0200

    Fixed undo for move

--- hojdoj/move_command.py
+++ hojdoj/move_command.py
@@ -18,23 +18,34 @@ class MoveCommand:
         if self.kwargs['index'] is None:
             self.sketch.mark_object(x, y)
             self.kwargs['index'] = self.sketch.marked_object_index
-
-        self.start_x, self.start_y = self.sketch.get_coords(self.kwargs['index'])
-        self.mark_x = x
-        self.mark_y = y
+        if self.kwargs['index'] is not None:
+            self.start_x, self.start_y = self.sketch.get_coords(self.kwargs['index'])
+            self.mark_x = x
+            self.mark_y = y
 
     def get_kwargs(self):
         return self.kwargs
 
     def on_move(self, x, y):
+        if self.kwargs['index'] is None:
+            return
         self.kwargs['dx'] = x - self.mark_x
         self.kwargs['dy'] = y - self.mark_y
         self.sketch.set_coords(self.kwargs['index'], self.start_x+self.kwargs['dx'], self.start_y+self.kwargs['dy'])
 
     def on_release(self, x, y):
+        if self.kwargs['index'] is None:
+            return
         self.kwargs['dx'] = x - self.mark_x
         self.kwargs['dy'] = y - self.mark_y
         self.sketch.add_command(self)
 
     def do(self):
+        if self.kwargs['index'] is None:
+            return
         self.sketch.set_coords(self.kwargs['index'], self.start_x + self.kwargs['dx'], self.start_y + self.kwargs['dy'])
+
+    def undo(self):
+        if self.kwargs['index'] is None:
+            return
+        self.sketch.set_coords(self.kwargs['index'], self.start_x, self.start_y)
