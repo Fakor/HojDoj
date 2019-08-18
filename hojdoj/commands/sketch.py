e348f26e3a80bf4bacf1b2516334c2a7219500bb
david <davidjohansson35@gmail.com>
2019-08-18 20:22:42 +0200

    Sketch command now use the upgraded kwargs usage

--- hojdoj/commands/sketch.py
+++ hojdoj/commands/sketch.py
@@ -1,4 +1,3 @@
-import inspect
 from DTools.sketch_image import SketchImage
 import DTools.fillers
 
@@ -6,28 +5,24 @@ import DTools.fillers
 class Command:
     name = 'sketch'
 
-    def __init__(self, sketch, index=None, event=None, image_meta=None,
-                 x=0, y=0, width=0,
-                 height=0, color=None,
+    def __init__(self, sketch, index=None, event=None, image_name=None,
+                 pos=(0,0), size=(0,0), color=None,
                  rotate=0, mirror=False, elastic_name=None):
-        _,_,_,self.kwargs = inspect.getargvalues(inspect.currentframe())
-
-        self.kwargs.pop('self')
-        self.kwargs.pop('sketch')
-        self.kwargs.pop('event')
-
         self.name = Command.name
         self.sketch = sketch
         if event:
-            self.start_x = event.x
-            self.start_y = event.y
-        else:
-            self.start_x = x
-            self.start_y = y
-        if self.kwargs['image_meta'] is None:
+            self.start_pos = (event.x, event.y)
+        self.pos = pos
+        if image_name is None:
             self.image_meta = self.sketch.current_image
         else:
-            self.image_meta = image_meta
+            self.image_meta = self.sketch.config.get_first_value('image_templates', name=image_name)
+        self.index = index
+        self.size = size
+        self.mirror = mirror
+        self.rotate = rotate
+        self.elastic_name = elastic_name
+        self.color = color
 
         if color is not None:
             self.filler = DTools.fillers.ColorFiller(color)
@@ -39,19 +34,28 @@ class Command:
 
         self.filler.get_arguments(self)
         try:
-            self.orig_image = self.sketch.objects[self.kwargs['index']]
+            self.orig_image = self.sketch.objects[self.index]
         except KeyError:
             self.orig_image = None
         self.image = SketchImage(sketch,
                                  self.image_meta['path'],
-                                 (self.start_x, self.start_y),
-                                 (width, height),
+                                 self.pos,
+                                 self.size,
                                  filler=self.filler,
                                  rotate=rotate,
                                  mirror=mirror)
 
     def get_kwargs(self):
-        return self.kwargs
+        return {
+            'index': self.index,
+            'image_name': self.image_meta['name'],
+            'pos': self.pos,
+            'size': self.size,
+            'rotate': self.rotate,
+            'mirror': self.mirror,
+            'color': self.color,
+            'elastic_name': self.elastic_name
+        }
 
     def on_move(self, event):
         self._prepare_shape(event.x, event.y)
@@ -59,16 +63,16 @@ class Command:
 
     def on_release(self, event):
         self._prepare_shape(event.x, event.y)
-        if self.kwargs['width'] == 0 or self.kwargs['height'] == 0:
+        if any(el == 0 for el in self.size):
             return
-        self.kwargs['index'] = self.sketch.next_image_index()
+        self.index = self.sketch.next_image_index()
         self.sketch.add_command(self)
 
     def do(self):
         if self.orig_image is not None:
             self.orig_image.hide()
         self._update_image()
-        self.sketch.objects[self.kwargs['index']] = self.image
+        self.sketch.objects[self.index] = self.image
 
     def undo(self):
         if self.orig_image is not None:
@@ -76,32 +80,20 @@ class Command:
         self.image.hide()
 
     def _prepare_shape(self, x, y):
-        self.kwargs['width'] = abs(x - self.start_x)
-        self.kwargs['height'] = abs(y - self.start_y)
+        self.size = (abs(x - self.start_pos[0]), abs(y - self.start_pos[1]))
 
-        if y < self.start_y:
-            self.kwargs['rotate'] = 180
-            self.kwargs['mirror'] = x > self.start_x
+        if y < self.start_pos[1]:
+            self.rotate = 180
+            self.mirror = x > self.start_pos[0]
         else:
-            self.kwargs['rotate'] = 0
-            self.kwargs['mirror'] = x < self.start_x
-        self.kwargs['x'] = int((x + self.start_x) / 2)
-        self.kwargs['y'] = int((y + self.start_y) / 2)
-
-    def _get_image_position(self):
-        return self.kwargs['x'], self.kwargs['y']
-
-    def _get_image_size(self):
-        return self.kwargs['width'], self.kwargs['height']
-
-    def _get_image_rotate(self):
-        return self.kwargs['rotate']
-
-    def _get_image_mirror(self):
-        return self.kwargs['mirror']
+            self.rotate = 0
+            self.mirror = x < self.start_pos[0]
+        new_x = int((x + self.start_pos[0]) / 2)
+        new_y = int((y + self.start_pos[1]) / 2)
+        self.pos = (new_x, new_y)
 
     def _update_image(self):
-        self.image.update(position=self._get_image_position(),
-                          size=self._get_image_size(),
-                          rotate=self._get_image_rotate(),
-                          mirror=self._get_image_mirror())
+        self.image.update(position=self.pos,
+                          size=self.size,
+                          rotate=self.rotate,
+                          mirror=self.mirror)
