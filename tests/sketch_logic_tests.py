e5a9df2c572252d8d90fa8bc365702ce1625c38f
david <davidjohansson35@gmail.com>
2019-08-30 20:31:57 +0200

    Fixed failing testcase

--- tests/sketch_logic_tests.py
+++ tests/sketch_logic_tests.py
@@ -13,17 +13,17 @@ class SketchLogicTests(unittest.TestCase):
         self.assertEqual(sketch.draw_object(draw_callback,'SQUARE', (0, 0), (10, 10)), (1, (0,0)))
         self.assertEqual(sketch.draw_object(draw_callback,'SQUARE', (0, 0), (10, 10), index=10), (10, (0,0)))
 
-        self.assertIsNone(sketch.mark_object(-15, 0))
-        self.assertIsNone(sketch.mark_object(0, -15))
-        self.assertIsNone(sketch.mark_object(15, 15))
+        self.assertIsNone(sketch.mark_object(mark_callback, (-15, 0)))
+        self.assertIsNone(sketch.mark_object(mark_callback, (0, -15)))
+        self.assertIsNone(sketch.mark_object(mark_callback, (15, 15)))
 
-        self.assertEqual(sketch.mark_object(0, 0), 10)
+        self.assertEqual(sketch.mark_object(mark_callback, (0, 0)), 10)
         sketch.delete_object(10)
-        self.assertEqual(sketch.mark_object(0, 0), 1)
+        self.assertEqual(sketch.mark_object(mark_callback, (0, 0)), 1)
         sketch.delete_object(1)
-        self.assertEqual(sketch.mark_object(0, 0), 0)
+        self.assertEqual(sketch.mark_object(mark_callback, (0, 0)), 0)
         sketch.delete_object(0)
-        self.assertIsNone(sketch.mark_object(0, 0))
+        self.assertIsNone(sketch.mark_object(mark_callback, (0, 0)))
 
     def test_move_object(self):
         sketch = SketchLogic(IMAGE_TEMPLATES)
