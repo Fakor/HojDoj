82e74f4a8287a45dbf1d1ac69d8ef9dc4d4ba7be
david <davidjohansson35@gmail.com>
2019-09-05 20:34:42 +0200

    Interactive resize more intuative

--- hojdoj/commands/resize.py
+++ hojdoj/commands/resize.py
@@ -8,8 +8,9 @@ class Command:
             return
         self.mark_x = event.x
         self.mark_y = event.y
-
-        self.org_size = self.sketch.get_object_size(self.index)
+        pos = self.sketch.get_object_position(self.index)
+        self.mark_on_right = pos[0] < self.mark_x
+        self.mark_on_up = pos[1] < self.mark_y
 
     def on_move(self, event):
         if self.index is None:
@@ -26,4 +27,10 @@ class Command:
                                 index=self.index)
 
     def get_dpos(self, x, y):
-        return 2*(self.mark_x - x), 2*(self.mark_y - y)
+        dx = 2*(self.mark_x - x)
+        if self.mark_on_right:
+            dx *= -1
+        dy = 2 * (self.mark_y - y)
+        if self.mark_on_up:
+            dy *= -1
+        return dx, dy
