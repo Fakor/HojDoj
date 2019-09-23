33088bcc6e69b835d37e74dddd5557d760629ae8
david <davidjohansson35@gmail.com>
2019-09-23 07:47:11 +0200

    Right button on jump now give instant action

--- hojdoj/commands/jump.py
+++ hojdoj/commands/jump.py
@@ -8,12 +8,21 @@ class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         BaseCommand.__init__(self, *args, **kwargs)
         self.mark_object()
+        if self.button2:
+            dx = np.sign(self.logic.object_position(self.index)[0]-self.init_event.x)*5
+            velocity = (dx, -20)
+            acceleration = (0, 1)
+            rng = count_jump_range(velocity, acceleration)
+            self.perform_command('motion',
+                                 velocity=velocity,
+                                 acceleration=acceleration,
+                                 range=rng)
 
     def on_move(self, event):
         pass
 
     def on_release(self, event):
-        if self.index is None:
+        if self.index is None or self.button2:
             return
         dpos = np.array(self.delta_position(event))
         velocity = tuple(dpos/20)
